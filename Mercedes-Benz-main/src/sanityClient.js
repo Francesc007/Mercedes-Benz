166a60b8ad0fcb8c06ac1518c1f39b70e03f9963
Francisco <fortizcg.007@gmail.com>
2025-12-28 22:13:24 -0600

    Actualizando exportación de Sanity y asegurando Project ID

--- Mercedes-Benz-main/src/sanityClient.js
+++ Mercedes-Benz-main/src/sanityClient.js
@@ -1,14 +1,14 @@
 import { createClient } from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'nfhji1ic',
-  dataset: import.meta.env.VITE_SANITY_PROJECT_ID ? import.meta.env.VITE_SANITY_DATASET : 'production',
+  dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
   useCdn: false, // `false` si quieres los datos más frescos siempre
   apiVersion: '2024-03-21', // Usa la fecha actual o una versión estable
 })
 
-const builder = imageUrlBuilder(client)
+const builder = createImageUrlBuilder(client)
 
 export function urlFor(source) {
   return builder.image(source)
