001e10a45f6a583dbf80b5470ea9435d5ac2dc6a
Francisco <fortizcg.007@gmail.com>
2025-12-28 19:28:34 -0600

    Actualizando cliente Sanity con fallback de ID

--- Mercedes-Benz-main/src/sanityClient.js
+++ Mercedes-Benz-main/src/sanityClient.js
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: 'nfhji1ic',
-  dataset: 'production',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'nfhji1ic',
+  dataset: import.meta.env.VITE_SANITY_PROJECT_ID ? import.meta.env.VITE_SANITY_DATASET : 'production',
   useCdn: false, // `false` si quieres los datos más frescos siempre
   apiVersion: '2024-03-21', // Usa la fecha actual o una versión estable
 })
